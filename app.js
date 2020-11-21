d472087ea2f41eda97d55718f4d9559cb8c6d119
seandelacerda <seanpdlc@gmail.com>
2020-11-21 00:24:41 +0000

    add nav and index content

--- app.js
+++ app.js
@@ -13,9 +13,16 @@ app.get("/mercury", function(req, res) {
 });
 
 app.get("/venus", function(req, res) {
-    res.send("This will be the Venus web page!");
+    res.render("venus.html");
 });
 
+app.get("/earth", function(req, res) {
+    res.render("earth.html");
+});
+
+app.get("/mars", function(req, res) {
+    res.render("mars.html");
+});
 
 // starting server
 app.listen(process.env.PORT, process.env.IP, function() {
